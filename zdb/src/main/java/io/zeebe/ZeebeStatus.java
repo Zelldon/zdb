b835e6b4efec74659880c24ab16784488d10a20b
Christopher Zell <zelldon91@googlemail.com>
2020-08-12 10:03:23 +0200

    add zeebestatus interface

---
+++ zdb/src/main/java/io/zeebe/ZeebeStatus.java
@@ -0,0 +1,20 @@
+package io.zeebe;
+
+import java.nio.file.Path;
+
+public interface ZeebeStatus {
+
+  /**
+   * Returns an well formated string which contains all status information about Zeebe,
+   * which is located under the given path.
+   *
+   * Contained information are:
+   *
+   *  * lastExportedPosition
+   *  * lastProcessedPosition
+   *
+   * @param path the partition path
+   * @return a well formated string which contains all information
+   */
+  public String status(Path path);
+}
