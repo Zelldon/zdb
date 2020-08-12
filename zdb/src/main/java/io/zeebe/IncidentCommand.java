9c74c229777faaa993084c933ce70d80cc090821
Nico korthout <nico.korthout@camunda.com>
2020-08-12 16:55:14 +0200

    chore(cli): support incident list and entry commands

--- zdb/src/main/java/io/zeebe/IncidentCommand.java
+++ zdb/src/main/java/io/zeebe/IncidentCommand.java
@@ -1,14 +1,14 @@
 package io.zeebe;
 
-import io.zeebe.impl.BlacklistInspection;
+import io.zeebe.impl.IncidentInspection;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.ScopeType;
 
-@Command(name = "blacklist", mixinStandardHelpOptions = true)
-public class BlackListCommand implements Callable<Integer> {
+@Command(name = "incident", mixinStandardHelpOptions = true)
+public class IncidentCommand implements Callable<Integer> {
 
   @Option(
       names = {"-p", "--path"},
@@ -20,28 +20,28 @@ public class BlackListCommand implements Callable<Integer> {
 
   @Override
   public Integer call() {
-    ZeebeDebugger.printUsage("blacklist");
+    ZeebeDebugger.printUsage("incident");
     return 0;
   }
 
-  @Command(name = "list", description = "List all blacklisted workflow instances")
+  @Command(name = "list", description = "List all incidents")
   public int list() {
     final var partitionState = PartitionState.of(partitionPath);
-    final var outputLines = new BlacklistInspection().list(partitionState);
+    final var outputLines = new IncidentInspection().list(partitionState);
     outputLines.forEach(System.out::println);
     return 0;
   }
 
-  @Command(name = "entry", description = "Shows details about blacklisted workflow instance")
+  @Command(name = "entry", description = "Shows details about an incident")
   public int entry(
       @Option(
               names = {"-k", "--key"},
               paramLabel = "KEY",
-              description = "The key of the blacklisted workflow instance",
+              description = "The key of the incident",
               required = true)
           final long key) {
     final var partitionState = PartitionState.of(partitionPath);
-    final var output = new BlacklistInspection().entity(partitionState, key);
+    final var output = new IncidentInspection().entity(partitionState, key);
     System.out.println(output);
     return 0;
   }
