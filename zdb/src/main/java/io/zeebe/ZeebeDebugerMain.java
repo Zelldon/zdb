d84fa1bfc999255fe910fca387378052c274043e
Christopher Zell <zelldon91@googlemail.com>
2020-08-12 11:25:59 +0200

    add formating and checkstyle

--- zdb/src/main/java/io/zeebe/ZeebeDebugerMain.java
+++ zdb/src/main/java/io/zeebe/ZeebeDebugerMain.java
@@ -5,21 +5,19 @@ import java.nio.file.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ZeebeDebugerMain
-{
-    private static final Logger LOGGER = LoggerFactory.getLogger(ZeebeDebugerMain.class);
+public class ZeebeDebugerMain {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ZeebeDebugerMain.class);
 
-    public static void main(String[] args) throws Exception
-    {
-       LOGGER.info("Zeebe Debug and inspection tool");
+  public static void main(String[] args) throws Exception {
+    LOGGER.info("Zeebe Debug and inspection tool");
 
-       // parse given parameters - exit with error code if necessary
-        final var partitionPath = Path.of(args[0]);
+    // parse given parameters - exit with error code if necessary
+    final var partitionPath = Path.of(args[0]);
 
-        // call corresponding command
-        final var result = new ZeebeStatusImpl().status(partitionPath);
+    // call corresponding command
+    final var result = new ZeebeStatusImpl().status(partitionPath);
 
-        // print result
-        LOGGER.info(result);
-    }
+    // print result
+    LOGGER.info(result);
+  }
 }
--- zdb/src/main/java/io/zeebe/ZeebeStatus.java
+++ zdb/src/main/java/io/zeebe/ZeebeStatus.java
@@ -5,13 +5,12 @@ import java.nio.file.Path;
 public interface ZeebeStatus {
 
   /**
-   * Returns an well formated string which contains all status information about Zeebe,
-   * which is located under the given path.
+   * Returns an well formated string which contains all status information about Zeebe, which is
+   * located under the given path.
    *
-   * Contained information are:
+   * <p>Contained information are:
    *
-   *  * lastExportedPosition
-   *  * lastProcessedPosition
+   * <p>* lastExportedPosition * lastProcessedPosition
    *
    * @param path the partition path
    * @return a well formated string which contains all information
