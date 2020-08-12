d6fe89ba6cb1e1976975e4aa3aa89eedac6a9cf3
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2020-08-12 11:42:30 +0200

    add processed and exported position to status

--- zdb/src/main/java/io/zeebe/impl/ZeebeStatusImpl.java
+++ zdb/src/main/java/io/zeebe/impl/ZeebeStatusImpl.java
@@ -2,13 +2,36 @@ package io.zeebe.impl;
 
 import io.zeebe.ZeebeStatus;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ZeebeStatusImpl implements ZeebeStatus {
+
+  private static Map<String, String> statuses = new HashMap<>();
 
   @Override
   public String status(final Path path) {
     final PartitionState partitionState = new PartitionState(path);
-    partitionState.getZeebeState().getLastSuccessfulProcessedRecordPosition();
-    return null;
+    lastProcessedPosition(partitionState);
+    lastExportedPosition(partitionState);
+    return getStatusString();
+  }
+
+  private void lastProcessedPosition(PartitionState partitionState) {
+    statuses.put(
+        "last processed position",
+        String.valueOf(partitionState.getZeebeState().getLastSuccessfulProcessedRecordPosition()));
+  }
+
+  private void lastExportedPosition(PartitionState partitionState) {
+    statuses.put(
+        "lowest exported position",
+        String.valueOf(partitionState.getExporterState().getLowestPosition()));
+  }
+
+  private String getStatusString() {
+    final var stringBuilder = new StringBuilder();
+    statuses.forEach((k, v) -> stringBuilder.append(String.format("%n%s: %s", k, v)));
+    return stringBuilder.toString();
   }
 }
