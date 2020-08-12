76e41cefa53f63f7e841fb7b352086d6c8df7064
Nico korthout <nico.korthout@camunda.com>
2020-08-12 16:18:51 +0200

    chore(cli): cleanup path option for blacklist

--- zdb/src/main/java/io/zeebe/BlackListCommand.java
+++ zdb/src/main/java/io/zeebe/BlackListCommand.java
@@ -5,9 +5,18 @@ import java.nio.file.Path;
 import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.ScopeType;
 
 @Command(name = "blacklist", mixinStandardHelpOptions = true)
 public class BlackListCommand implements Callable<Integer> {
+
+  @Option(
+      names = {"-p", "--path"},
+      paramLabel = "PARTITION_PATH",
+      description = "The path to the partition data (either runtime or snapshot in partition dir)",
+      required = true,
+      scope = ScopeType.INHERIT)
+  private Path partitionPath;
 
   @Override
   public Integer call() {
@@ -15,17 +24,8 @@ public class BlackListCommand implements Callable<Integer> {
     return 0;
   }
 
-  @Command(
-      name = "list",
-      description = "List all blacklisted workflow instances")
-  public int list(
-      @Option(
-              names = {"-p", "--path"},
-              paramLabel = "PARTITION_PATH",
-              description =
-                  "The path to the partition data (either runtime or snapshot in partition dir)",
-              required = true)
-          Path partitionPath) {
+  @Command(name = "list", description = "List all blacklisted workflow instances")
+  public int list() {
     final var partitionState = PartitionState.of(partitionPath);
     final var outputLines = new BlacklistInspection().list(partitionState);
     outputLines.forEach(System.out::println);
@@ -34,13 +34,6 @@ public class BlackListCommand implements Callable<Integer> {
 
   @Command(name = "entry", description = "Shows details about blacklisted workflow instance")
   public int entry(
-      @Option(
-              names = {"-p", "--path"},
-              paramLabel = "PARTITION_PATH",
-              description =
-                  "The path to the partition data (either runtime or snapshot in partition dir)",
-              required = true)
-          Path partitionPath,
       @Option(
               names = {"-k", "--key"},
               description = "The key of the blacklisted workflow instance",
