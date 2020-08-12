dc2a6cdaebdd25e76339a7acfc80939471e98487
Christopher Zell <zelldon91@googlemail.com>
2020-08-12 10:00:32 +0200

    add project

---
+++ zdb/src/main/java/io/zeebe/ZeebeDebugerMain.java
@@ -0,0 +1,13 @@
+package io.zeebe;
+
+/**
+ * Hello world!
+ *
+ */
+public class ZeebeDebugerMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
