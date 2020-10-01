01ec0278a570329e064803fef09769bf867ff046
Christopher Zell <zelldon91@googlemail.com>
2020-10-01 16:11:31 +0200

    chore(test): update to next version

--- test.sh
+++ test.sh
@@ -4,18 +4,20 @@ trap 'echo Terminated $0; exit' INT;
 
 set -x
 
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar &&
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar status --path=database/data/raft-partition/partitions/1/runtime &&
+version=0.2.0-SNAPSHOT
 
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar blacklist --path=database/data/raft-partition/partitions/1/runtime list &&
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar blacklist --path=database/data/raft-partition/partitions/1/runtime entity 2251799813685307 &&
+java -jar zdb/target/zdb-$version.jar &&
+java -jar zdb/target/zdb-$version.jar status --path=database/data/raft-partition/partitions/1/runtime &&
 
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar incident --path=database/data/raft-partition/partitions/1/runtime list &&
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar incident --path=database/data/raft-partition/partitions/1/runtime entity 2251799813685269 &&
+java -jar zdb/target/zdb-$version.jar blacklist --path=database/data/raft-partition/partitions/1/runtime list &&
+java -jar zdb/target/zdb-$version.jar blacklist --path=database/data/raft-partition/partitions/1/runtime entity 2251799813685307 &&
 
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar workflow --path=database/data/raft-partition/partitions/1/runtime list &&
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar workflow --path=database/data/raft-partition/partitions/1/runtime entity 2251799813685305 &&
+java -jar zdb/target/zdb-$version.jar incident --path=database/data/raft-partition/partitions/1/runtime list &&
+java -jar zdb/target/zdb-$version.jar incident --path=database/data/raft-partition/partitions/1/runtime entity 2251799813685269 &&
 
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar log status --path=database/data/raft-partition/partitions/1/ &&
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar log consistency --path=database/data/raft-partition/partitions/1 &&
-java -jar zdb/target/zdb-0.1.0-SNAPSHOT.jar log search --path=database/data/raft-partition/partitions/1 -pos=4294967296
+java -jar zdb/target/zdb-$version.jar workflow --path=database/data/raft-partition/partitions/1/runtime list &&
+java -jar zdb/target/zdb-$version.jar workflow --path=database/data/raft-partition/partitions/1/runtime entity 2251799813685305 &&
+
+java -jar zdb/target/zdb-$version.jar log status --path=database/data/raft-partition/partitions/1/ &&
+java -jar zdb/target/zdb-$version.jar log consistency --path=database/data/raft-partition/partitions/1 &&
+java -jar zdb/target/zdb-$version.jar log search --path=database/data/raft-partition/partitions/1 -pos=4294967296
