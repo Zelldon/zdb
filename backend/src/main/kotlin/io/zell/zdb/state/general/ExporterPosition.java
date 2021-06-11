fb37f2a3c8efbb26f2877a0684aaed17e544c51e
Christopher Zell <zelldon91@googlemail.com>
2021-06-11 12:01:31 +0200

    feat: add exporter position
    used for exporter details

---
+++ backend/src/main/kotlin/io/zell/zdb/state/general/ExporterPosition.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.zell.zdb.state.general;
+
+import io.camunda.zeebe.db.DbValue;
+import io.camunda.zeebe.msgpack.UnpackedObject;
+import io.camunda.zeebe.msgpack.property.LongProperty;
+
+/**
+ * *NOTE: COPIED FROM ZEEBE BROKER*
+ */
+public class ExporterPosition extends UnpackedObject implements DbValue {
+  private final LongProperty positionProp = new LongProperty("exporterPosition");
+
+  public ExporterPosition() {
+    declareProperty(positionProp);
+  }
+
+  public void set(final long position) {
+    positionProp.setValue(position);
+  }
+
+  public long get() {
+    return positionProp.getValue();
+  }
+}
