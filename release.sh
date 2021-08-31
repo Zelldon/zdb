8896576b5a3d00e992d454d062c837760293e8a3
Christopher Zell <zelldon91@googlemail.com>
2021-08-31 11:03:37 +0200

    build: add release script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+set -euxo pipefail
+
+version=$1
+nextVersion=$2
+
+git checkout master
+git pull origin master
+
+mvn versions:set "-DnewVersion=$version"
+mvn clean install -DskipTests
+
+git commit -am "build: release version $version"
+git tag "$version"
+
+mvn versions:set "-DnewVersion=$nextVersion-SNAPSHOT"
+
+git commit -am "build: prepare for next version"
+
+cp cli/target/cli-*-jar-with-dependencies.jar zdb.jar
+
+
