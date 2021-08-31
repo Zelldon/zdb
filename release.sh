5d637643d6c7b336b28575cee5ce8b72a48c91c8
Christopher Zell <zelldon91@googlemail.com>
2021-08-31 11:19:31 +0200

    fix: no backup files

--- release.sh
+++ release.sh
@@ -9,13 +9,13 @@ nextVersion=$2
 git checkout master
 git pull origin master
 
-mvn versions:set "-DnewVersion=$version"
+mvn versions:set "-DnewVersion=$version" -DgenerateBackupPoms=false
 mvn clean install -DskipTests
 
 git commit -am "build: release version $version"
 git tag "$version"
 
-mvn versions:set "-DnewVersion=$nextVersion-SNAPSHOT"
+mvn versions:set "-DnewVersion=$nextVersion-SNAPSHOT" -DgenerateBackupPoms=false
 
 git commit -am "build: prepare for next version"
 
