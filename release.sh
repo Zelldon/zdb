f492b07f48d88b3beea40ac8e6c8d00f053af922
Christopher Zell <zelldon91@googlemail.com>
2021-11-10 09:25:43 +0100

    docs: add hints to release script

--- release.sh
+++ release.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 
-set -euxo pipefail
+set -exo pipefail
+
+if [ -z "$1" ] 
+then
+  echo "Provide a version: ./release releaseVersion nextVersion"
+  exit 1
+fi
+
+
+if [ -z "$2" ] 
+then
+  echo "Provide a version: ./release releaseVersion nextVersion"
+  exit 1
+fi
 
 version=$1
 nextVersion=$2
