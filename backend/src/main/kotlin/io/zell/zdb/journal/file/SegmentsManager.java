41f43d06421534bdf24a0b60f46fd6787b8d4a4c
Christopher Zell <zelldon91@googlemail.com>
2023-07-09 22:12:28 +0200

    fix: do not throw on empty

--- backend/src/main/kotlin/io/zell/zdb/journal/file/SegmentsManager.java
+++ backend/src/main/kotlin/io/zell/zdb/journal/file/SegmentsManager.java
@@ -116,8 +116,6 @@ final class SegmentsManager implements AutoCloseable {
     // If a segment doesn't already exist, create an initial segment starting at index 1.
     if (!segments.isEmpty()) {
       currentSegment = segments.lastEntry().getValue();
-    } else {
-      throw new IllegalStateException("Expected to read segments, but there was nothing to read.");
     }
   }
 
