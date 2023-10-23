c3f2f471d5bf580a4ee8093257f739b39a662ce9
Christopher Zell <zelldon91@googlemail.com>
2023-10-23 23:47:58 +0200

    docs: add license

--- backend/src/test/kotlin/io/zell/zdb/v83/Version83Test.java
+++ backend/src/test/kotlin/io/zell/zdb/v83/Version83Test.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2021 Christopher Kujawa (zelldon91@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.zell.zdb.v83;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -14,12 +29,14 @@ import io.zeebe.containers.ZeebeContainer;
 import io.zell.zdb.TestUtils;
 import io.zell.zdb.ZeebeContentCreator;
 import io.zell.zdb.ZeebePaths;
-import io.zell.zdb.log.*;
+import io.zell.zdb.log.LogContentReader;
+import io.zell.zdb.log.LogSearch;
+import io.zell.zdb.log.LogStatus;
+import io.zell.zdb.log.LogWriter;
 import io.zell.zdb.log.records.ApplicationRecord;
 import io.zell.zdb.log.records.PersistedRecord;
 import io.zell.zdb.log.records.RaftRecord;
 import io.zell.zdb.log.records.Record;
-import io.zell.zdb.log.records.RecordValue;
 import io.zell.zdb.state.ZeebeDbReader;
 import io.zell.zdb.state.instance.InstanceState;
 import io.zell.zdb.state.process.ProcessState;
